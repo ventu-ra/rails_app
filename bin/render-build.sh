65f6169ab0b2bd334eff04ff1cd16689adda428e
ventu-ra <ventura09.10.0@gmail.com>
2024-08-08 17:44:12 -0400

    first commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash# exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Instala as dependências Ruby
 bundle install
+
+# Pré-compila os assets
 bundle exec rails assets:precompile
+
+# Limpa os assets antigos
 bundle exec rails assets:clean
